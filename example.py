d590d94e1cfb349aeaa3df5375146d68b8b2c258
Jun Hao <diaryofnotes@gmail.com>
2019-04-19 23:29:07 +0800

    Update weights

--- example.py
+++ example.py
@@ -6,12 +6,12 @@ from raise_player import RaisedPlayer
 from Group02PlayerConfig import Group02Player
 
 #TODO:config the config as our wish
-config = setup_config(max_round=500, initial_stack=10000, small_blind_amount=20)
+config = setup_config(max_round=100, initial_stack=10000, small_blind_amount=20)
 
 config.register_player(name="FT1", algorithm=RaisedPlayer())
 # config.register_player(name="AI_2", algorithm=Group02Player(np.array([0.44581346, 0.30448666, 0.24969988])))
-w1 = 0.499971
-w2 = 0.1
+w1 = 0.8
+w2 = 0.05
 w3 = 1 - w1 - w2
 config.register_player(name="AI_2", algorithm=Group02Player(np.array([w1, w2, w3])))
 
