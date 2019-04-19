8d76ac818604a9bb827caac81429d1ac8f2485ac
Jun Hao <diaryofnotes@gmail.com>
2019-04-19 23:40:15 +0800

    Change back to 500 rounds

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ from raise_player import RaisedPlayer
 from Group02PlayerConfig import Group02Player
 
 #TODO:config the config as our wish
-config = setup_config(max_round=100, initial_stack=10000, small_blind_amount=20)
+config = setup_config(max_round=500, initial_stack=10000, small_blind_amount=20)
 
 config.register_player(name="FT1", algorithm=RaisedPlayer())
 # config.register_player(name="AI_2", algorithm=Group02Player(np.array([0.44581346, 0.30448666, 0.24969988])))
